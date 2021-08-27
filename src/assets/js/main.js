941b6b562931814e014a134d98affe98e846db1a
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-08-27 15:30:36 +0900

    create Step7

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,6 @@
+// developer mode
+Vue.config.devtools = true;
+
 // STEP 2
 // https://jp.vuejs.org/v2/examples/todomvc.html
 let STORAGE_KEY = 'todos-vuejs-demo'
@@ -21,7 +24,32 @@ let todoStorage = {
 const app = new Vue({
   el: '#app',
   data: {
+    todos: []
   },
+
   methods: {
-  }
+    // アロー関数を使うとthisの値を参照できない
+    doAdd: function(event, value) {
+      // $refs.comment で値を取得する
+      let comment = this.$refs.comment
+      if (!comment.value.length) { return }
+      this.todos.push({
+        id: todoStorage.uid++,
+        comment: comment.value,
+        state: 0
+      })
+      comment.value = ''
+    }
+  },
+
+  // ローカルストレージに保存
+  watch: {
+    todos: {
+      // アロー関数を使うとthisの値を参照できない
+      handler: function(todos) {
+        todoStorage.save(todos)
+      },
+      deep: true
+    }
+  },
 })
